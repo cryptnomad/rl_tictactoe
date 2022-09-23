f40476c083d374fdaa1ce062f9060eaadc1823a3
Adam Rankin <92758517+cryptnomad@users.noreply.github.com>
2022-09-23 17:36:27 +0000

    Fixed replit bs and warning for unused var

--- src/human_game.rs
+++ src/human_game.rs
@@ -1,8 +1,7 @@
 use std::io;
-use std::ptr;
 
-use crate::state::State;
 use crate::ai_player::AiPlayer;
+use crate::state::State;
 
 #[derive(Debug)]
 pub struct HumanGame {
@@ -13,33 +12,37 @@ pub struct HumanGame {
 }
 
 impl HumanGame {
-    pub fn new(human_symbol: isize) -> Self{
-        let ai_player = AiPlayer::competitor(human_symbol*-1);
-        Self{
+    pub fn new(human_symbol: isize) -> Self {
+        let ai_player = AiPlayer::competitor(human_symbol * -1);
+        Self {
             current_state: State::new(),
             current_symbol: 1,
             ai_player: ai_player,
-            human_symbol: human_symbol
+            human_symbol: human_symbol,
         }
     }
 
-    pub fn play(&mut self) -> isize{
+    pub fn play(&mut self) -> isize {
         let mut input_str = String::new();
         self.ai_player.set_state(&self.current_state);
         println!("\nYou will use the number keys to select your move.\n");
         println!("The board is set up like so:");
         println!("0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\n",);
         println!("Press enter when ready\n");
-        io::stdin().read_line(ptr::null_mut());
-        
-        while !self.current_state.is_end(){
+        io::stdin()
+            .read_line(&mut input_str)
+            .expect("Failed to read input");
+        input_str.clear();
+
+        while !self.current_state.is_end() {
             self.current_state.display();
-            let index = if self.current_symbol == self.human_symbol{
+            let index = if self.current_symbol == self.human_symbol {
                 self.get_move()
-            }else{
+            } else {
                 self.ai_player.act()
             };
-            self.current_state = self.current_state
+            self.current_state = self
+                .current_state
                 .get_next_state(index, self.current_symbol);
             self.ai_player.set_state(&self.current_state);
             self.alternate();
@@ -48,10 +51,10 @@ impl HumanGame {
         return self.current_state.get_winner();
     }
 
-    fn get_move(&self) -> usize{
+    fn get_move(&self) -> usize {
         let mut move_str: String;
         let mut move_index: usize = 9;
-        while !self.move_is_valid(move_index){
+        while !self.move_is_valid(move_index) {
             move_str = "".to_string();
             io::stdin()
                 .read_line(&mut move_str)
@@ -62,14 +65,14 @@ impl HumanGame {
         move_index
     }
 
-    fn move_is_valid(&self, index: usize) -> bool{
-        if index > 8{
+    fn move_is_valid(&self, index: usize) -> bool {
+        if index > 8 {
             return false;
         }
         self.current_state.get_tile(index) == 0
     }
 
-    fn alternate(&mut self){
-        self.current_symbol = self.current_symbol*-1;
+    fn alternate(&mut self) {
+        self.current_symbol = self.current_symbol * -1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+/*
+    Author: @cryptnomad
+*/
+pub mod ai_game;
+pub mod ai_player;
+pub mod human_game;
+pub mod state;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's teach some ai tic tac toe.");
+    ai_game::train(100000, 10000);
+
+    // Should be a draw every time if sufficient training has occured
+    ai_game::compete(500);
+
+    let mut input_str = String::new();
+    println!("\n\nDo you want to play against the models? (y/n)");
+
+    io::stdin()
+        .read_line(&mut input_str)
+        .expect("Failed to read the line");
+
+    if input_str.trim() == "Y" || input_str.trim() == "y" {
+        loop {
+            let mut symbol_select = String::new();
+            while symbol_select.trim() != "X" && symbol_select.trim() != "O" {
+                symbol_select = "".to_string();
+                println!("Would you like to play X (first) or O (second)? (X/O)");
+                io::stdin()
+                    .read_line(&mut symbol_select)
+                    .expect("Failed to read the line");
+            }
+
+            let user_symbol: isize = match symbol_select.trim() {
+                "X" => 1,
+                "O" => -1,
+                _ => 1,
+            };
+            let mut game = human_game::HumanGame::new(user_symbol);
+            let winner = game.play();
+
+            if winner == user_symbol {
+                println!("You win!");
+            } else if winner == 0 {
+                println!("Draw!");
+            } else {
+                println!("You lose! ")
+            }
+        }
+    }
+
+    println!("\nGoodbye!");
 }
